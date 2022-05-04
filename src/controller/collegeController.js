c0e73fadcf758e17554e40093e2ab22406aa73b3
Dheeraj Kumar Saw <dheerajkumarsaw99@gmail.com>
2022-05-04 19:07:10 +0530

    project done

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -15,6 +15,8 @@ const collegeCreate = async function (req, res) {
         if (!isValid(name)) return res.status(400).send({ status: false, message: "Name is Required" });
         if (!isValid(fullName)) return res.status(400).send({ status: false, message: "FullName is Required" });
         if (!isValid(logoLink)) return res.status(400).send({ status: false, message: "LogoLink is required" });
+        const regx= /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/+#-]*[\w@?^=%&amp;\/+#-])?/;
+        if (!regx.test(logoLink)) return res.status(400).send({status:false,message:"Enter Valid Url"})
 
         const unique = await collegeModel.findOne({ name: name });
         if (unique) return res.status(401).send({ status: false, message: "College Allready Exist" })
