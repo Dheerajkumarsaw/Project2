8f42a38162839d2a55e94d0a1e73cb02ee8104df
Dheeraj Kumar Saw <dheerajkumarsaw99@gmail.com>
2022-05-05 07:20:46 +0530

    final done

--- uranium/src/model/internModel.js
+++ uranium/src/model/internModel.js
@@ -3,10 +3,9 @@ const ObjectId = mongoose.Schema.Types.ObjectId
 
 const internSchema = new mongoose.Schema({
     name: { type: String, required: true, trim: true },
-    email: { type: String, required: true, unique: true, trim: true },
+    email: { type: String, required: true, trim: true },
     mobile: {
         type: String,
-        unique: true,
         required: true,
         minlength: 10, maxlength: 10,
         trim: true
