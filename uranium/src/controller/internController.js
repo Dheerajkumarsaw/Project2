0651297a6e25a203225ac086d536da0d8b0db8bf
Dheeraj Kumar Saw <dheerajkumarsaw99@gmail.com>
2022-05-05 20:51:23 +0530

    final done

--- uranium/src/controller/internController.js
+++ uranium/src/controller/internController.js
@@ -15,9 +15,8 @@ const createIntern = async function (req, res) {
         if (Object.keys(requestBody).length == 0) return res.status(400).send({ status: false, message: "Enter Student Details" });
 
         let { name, mobile, email, collegeName } = requestBody; //destructuring
-
+        // validations
         if (!isValid(name)) return res.status(400).send({ status: false, message: "Name is required" });
-
         if (!isValid(email)) return res.status(400).send({ status: false, message: "Email is Required" });
         const regx = /^([a-z0-9\._]+)@([a-z0-9]+.)([a-z]+)(.[a-z])?$/;
         if (!regx.test(email)) return res.status(400).send({ status: false, message: "Enter Valid Email" });
@@ -25,15 +24,15 @@ const createIntern = async function (req, res) {
         if (!isValid(mobile)) return res.status(400).send({ status: false, message: "Mobile no is required" });
         const mobileRegx = /^(\+\d{1,3}[- ]?)?\d{10}$/;
         if (!mobileRegx.test(mobile)) return res.status(400).send({ status: false, message: "Enter Valid Mobile no" })
-
         if (!isValid(collegeName)) return res.status(400).send({ status: false, message: "College Name is Required" });
-
+        //end
         const college = await collegeModel.findOne({ name: collegeName });
         if (!college) return res.status(404).send({ status: false, message: `${collegeName} Not Exist Register First` });
         const collegeId = college._id;
 
-        const unique = await internModel.findOne({ email: email, mobile: mobile });
-        if (unique) return res.status(400).send({ status: false, message: "Use Different Email or Password" }); ////change status code
+        let unique = await internModel.findOne({ email: email }); //checkine unique by email
+        unique = await internModel.findOne({ mobile: mobile }) //checkine unique by mobile
+        if (unique) return res.status(400).send({ status: false, message: "Use Different Email or Mobile No" }); ////change status code
 
         requestBody["collegeId"] = collegeId;
         const internCreate = await internModel.create(requestBody);
@@ -50,7 +49,7 @@ const createIntern = async function (req, res) {
 const collegeDetails = async function (req, res) {
     try {
         const collegeName = req.query.collegeName
-        if (Object.keys(collegeName).length === 0) return res.status(400).send({ status: false, message: "Query not Received" });
+        if (!collegeName) return res.status(400).send({ status: false, message: "Query not Received" });
 
         const college = await collegeModel.findOne({ name: collegeName })
         if (!college) return res.status(404).send({ status: false, message: `${collegeName} Not Found, Register First` });
@@ -59,11 +58,11 @@ const collegeDetails = async function (req, res) {
         data["interests"] = [];
         const modelCollegeId = college._id;
 
-        const internList = await internModel.find({ collegeId: modelCollegeId }).select({_id:1,name:1,email:1,mobile:1});
-        
+        const internList = await internModel.find({ collegeId: modelCollegeId }).select({ _id: 1, name: 1, email: 1, mobile: 1 });
+
         if (internList.length == 0) return res.status(404).send({ status: false, message: `${collegeName} Not Have Any Internship` });
-            data["interests"] = [...internList]
-        res.status(200).send({ status: true, data:data });
+        data["interests"] = [...internList]
+        res.status(200).send({ status: true, data: data });
 
     } catch (err) {
         res.status(500).send({ status: false, message: err.message })
